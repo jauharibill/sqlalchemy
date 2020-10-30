b87bce00e6a84cf7825173fd8cdc7ab4e21526b2
Bill Tanthowi Jauhari <bill.tanthowi@majoo.id>
2020-10-30 18:16:31 +0700

    add update method in user module

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from controllers.student import StudentController
 user = UserController()
 student = StudentController()
 
+user_data = ["dian romadlonal", "adzim"]
+
 if __name__ == "__main__":
-    user.index()
-    student.index()
+    user.update(2, user_data)
